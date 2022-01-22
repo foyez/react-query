abdbe9c7eb8a462279d4401243f5bd5263b46dff
foyez <foyezar@gmail.com>
2022-01-22 11:10:11 +0600

    add ReactQueryDevtools

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+import "../styles/globals.css";
 import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -11,6 +13,8 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Layout>
         <Component {...pageProps} />
       </Layout>
+
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
     </QueryClientProvider>
   );
 }
